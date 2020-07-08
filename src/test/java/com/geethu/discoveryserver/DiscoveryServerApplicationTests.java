228c7cd89890f0898645e5f5b1f3888949d56a23
Geethu Unnikrishnan <gunnikrishnan@expedia.com>
2020-07-07 23:07:41 -0700

    Initial commit

---
+++ src/test/java/com/geethu/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geethu.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
